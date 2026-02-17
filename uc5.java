0a6430b8ffaf5588cc5c21d31e2034fb0ee546a5
rithik-at-2007 <ra2753@srmist.edu.in>
2026-02-17 10:14:27 +0530

    use case 5

---
+++ uc5.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class uc5 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Create Stack
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Pop characters and compare
+        for (int i = 0; i < input.length(); i++) {
+
+            char poppedChar = stack.pop();
+
+            if (input.charAt(i) != poppedChar) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a Palindrome ✅");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome ❌");
+        }
+
+        scanner.close();
+    }
+}
