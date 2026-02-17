7f7cbba9f726822f6d3018a8a4db65f0033d378c
rithik-at-2007 <ra2753@srmist.edu.in>
2026-02-17 09:23:34 +0530

    uc1.java

---
+++ Documents/RA2411003011377/1/src/uc1.java
@@ -0,0 +1,16 @@
+public class uc1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("      Welcome to Palindrome Checker  ");
+        System.out.println("=====================================");
+
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0.0");
+        System.out.println("Author           : Your Name Here");
+        System.out.println("-------------------------------------");
+
+        System.out.println("Application started successfully!");
+    }
+}
---
+++ Documents/RA2411003011377/1/src/uc4.java
@@ -0,0 +1,31 @@
+public class uc4 {
+
+    public static void main(String[] args) {
+
+        String original = "radar";
+
+        char[] characters = original.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string \"" + original + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + original + "\" is NOT a Palindrome.");
+        }
+    }
+}
