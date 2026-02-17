d52a843431b01aae9158a507681a5cb655518f57
rithik-at-2007 <ra2753@srmist.edu.in>
2026-02-17 10:09:48 +0530

    use case 4

---
+++ uc4.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class uc4 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        char[] characters = input.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome ✅");
+        } else {
+            System.out.println("Not a Palindrome ❌");
+        }
+
+        scanner.close();
+    }
+}
